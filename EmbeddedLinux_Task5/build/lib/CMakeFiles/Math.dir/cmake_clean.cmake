c6cb898aa52de8dac3a7aefc0eb63577f572acb5
Mohammed-Ebrahim-Ahmed <mohammed.ebrahim.hassanin@gmail.com>
2024-01-05 03:00:49 +0200

    adding the cmake task

---
+++ EmbeddedLinux_Task5/build/lib/CMakeFiles/Math.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Math.dir/addition.c.o"
+  "CMakeFiles/Math.dir/addition.c.o.d"
+  "CMakeFiles/Math.dir/division.c.o"
+  "CMakeFiles/Math.dir/division.c.o.d"
+  "CMakeFiles/Math.dir/modulus.c.o"
+  "CMakeFiles/Math.dir/modulus.c.o.d"
+  "CMakeFiles/Math.dir/multiplication.c.o"
+  "CMakeFiles/Math.dir/multiplication.c.o.d"
+  "CMakeFiles/Math.dir/subtraction.c.o"
+  "CMakeFiles/Math.dir/subtraction.c.o.d"
+  "libMath.a"
+  "libMath.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Math.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
